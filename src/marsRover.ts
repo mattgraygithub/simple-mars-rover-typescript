6c2e6becd4ccbb161eae282a2bb93b90b31dcb9d
Matt Gray <matt.gray@ovo.com>
2025-01-14 17:24:55 +0000

    Further refactoring - extracted new position formatting

--- src/marsRover.ts
+++ src/marsRover.ts
@@ -7,35 +7,35 @@ type POSITION_ARGS = {
 const DIRECTIONS = ['N', 'E', 'S', 'W'];
 const POSITION = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-const next = (item: string, array: string[]) => {
-    return array[(array.indexOf(item) + 1) % array.length];
-};
+const formattedNewPosition = (x: string, y: string, direction: string) => `${x}:${y}:${direction}`;
+
+const next = (item: string, array: string[]) => array[(array.indexOf(item) + 1) % array.length];
 
 const previous = (item: string, array: string[]) => {
     const arrayReversed = array.slice().reverse();
     return arrayReversed[(arrayReversed.indexOf(item) + 1) % array.length];
 };
 
-const rotateLeft = ({x, y, direction}: POSITION_ARGS) => {
-    return `${x}:${y}:${previous(direction, DIRECTIONS)}`;
+const rotateRight = ({x, y, direction}: POSITION_ARGS) => {
+    return formattedNewPosition(x, y, next(direction, DIRECTIONS));
 };
 
-const rotateRight = ({x, y, direction}: POSITION_ARGS) => {
-    return `${x}:${y}:${next(direction, DIRECTIONS)}`;
+const rotateLeft = ({x, y, direction}: POSITION_ARGS) => {
+    return formattedNewPosition(x, y, previous(direction, DIRECTIONS));
 };
 
 const move = ({x, y, direction}: POSITION_ARGS) => {
     switch (direction) {
         case 'N':
-            return `${x}:${next(y, POSITION)}:${direction}`;
+            return formattedNewPosition(x, next(y, POSITION), direction);
         case 'S':
-            return `${x}:${previous(y, POSITION)}:${direction}`;
+            return formattedNewPosition(x, previous(y, POSITION), direction);
         case 'E':
-            return `${next(x, POSITION)}:${y}:${direction}`;
+            return formattedNewPosition(next(x, POSITION), y, direction);
         case 'W':
-            return `${previous(x, POSITION)}:${y}:${direction}`;
+            return formattedNewPosition(previous(x, POSITION), y, direction);
         default:
-            return `${x}:${y}:${direction}`;
+            return formattedNewPosition(x, y, direction);
     }
 };
 
@@ -47,10 +47,10 @@ export const execute = (commands: string) => {
             direction: position.split(":")[2]
         }
         switch (command) {
-            case 'L':
-                return rotateLeft(positionArgs);
             case 'R':
                 return rotateRight(positionArgs);
+            case 'L':
+                return rotateLeft(positionArgs);
             case 'M':
                 return move(positionArgs);
         }
